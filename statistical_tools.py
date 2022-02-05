edbbe0106deb876d1089c6bf148eac54e3dcb71a
Aleksey <alexei.veselov92@gmail.com>
2022-02-06 02:34:48 +0300

    update

--- statistical_tools.py
+++ statistical_tools.py
@@ -41,13 +41,17 @@ def get_fraction_ci(successes, n, conf_level = 0.95):
         print('Please, choose one of 3 values of conf_level: 0.9, 0.95 or 0.99')
 # стат. тестирование методом бутстрепа для двух выборок с графиком и таблицей результатов
 def bootstrap_test(
-    data_column_1, # числовые значения первой выборки
-    data_column_2, # числовые значения второй выборки
+    values_1, # числовые значения первой выборки
+    values_2, # числовые значения второй выборки
     n_samples = 1000, # количество бутстрэп-подвыборок
     statistic_func = 'mean', # интересующая нас статистика
     percentile_value = 50,
     bootstrap_conf_level = 0.95, # уровень значимости
+    stratified = None,
+    categories_1 = None,
+    categories_2 = None,
     chart = True,
+    chart_type = 'compare',
     print_results = True,
     returns = 'results'):
     '''
@@ -55,17 +59,53 @@ def bootstrap_test(
 
     returns: str - 'boot_data', 'quants', 'p_value', 'results'
     '''
-    boot_len = max([len(data_column_1), len(data_column_2)])
-    indices = np.random.randint(0, len(data_column_1), (n_samples, boot_len))
-    samples_1 = data_column_1.values[indices]
-    indices = np.random.randint(0, len(data_column_2), (n_samples, boot_len))
-    samples_2 = data_column_2.values[indices]
+    if stratified == True:
+        if type(categories_1) not in [pd.core.series.Series, list, np.ndarray] or type(categories_2) not in [pd.core.series.Series, list, np.ndarray]:
+            print('Error! For stratified bootstrap you need to use categories_1 and categories_2 as pd.Series, list of np.array of categorial values')
+            return
+        else:
+            data = pd.DataFrame()
+            for values_n in [1,2]:
+                if values_n == 1: df = pd.DataFrame({'values':values_1, 'categories':categories_1})
+                if values_n == 2: df = pd.DataFrame({'values':values_2, 'categories':categories_2})
+                df['values_n'] = values_n
+                data = pd.concat([data, df])
+            max_values_in_categories = pd.concat([
+                categories_1.value_counts().rename('count'),
+                categories_2.value_counts().rename('count')
+            ]).reset_index().rename(columns = {'index':'category'}).groupby('category')['count'].max().reset_index()
+            for index, row in max_values_in_categories.iterrows():
+                boot_len = row['count']
+                category = row['category']
+                cat_values_1 = data.query('values_n==1 and categories==@category')['values']
+                cat_values_2 = data.query('values_n==2 and categories==@category')['values']
+
+                indices = np.random.randint(0, len(cat_values_1), (n_samples, boot_len))
+                if index == 0: samples_1 = cat_values_1.values[indices]
+                if index != 0: samples_1 = np.concatenate([samples_1, cat_values_1.values[indices]], axis = 1)
+
+                indices = np.random.randint(0, len(cat_values_2), (n_samples, boot_len))
+                if index == 0: samples_2 = cat_values_2.values[indices]
+                if index != 0: samples_2 = np.concatenate([samples_2, cat_values_2.values[indices]], axis = 1)
+    else:           
+        boot_len = max([len(values_1), len(values_2)])
+        indices = np.random.randint(0, len(values_1), (n_samples, boot_len))
+        samples_1 = values_1.values[indices]
+        indices = np.random.randint(0, len(values_2), (n_samples, boot_len))
+        samples_2 = values_2.values[indices]
     
     if statistic_func=='mean':
-        boot_data = list(map(lambda x: np.mean(x), samples_1-samples_2))    
+        boot_data = list(map(lambda x: np.mean(x), samples_1-samples_2))
+        boot_mean_data_1 = list(map(lambda x: np.mean(x), samples_1))
+        boot_mean_data_2 = list(map(lambda x: np.mean(x), samples_2))
+        bootstrapped_value_1 = np.mean(boot_mean_data_1)
+        bootstrapped_value_2 = np.mean(boot_mean_data_2)
     else:
-        boot_data = list(map(lambda x: np.percentile(x, q=percentile_value), samples_1-samples_2))    
-
+        boot_data = list(map(lambda x: np.percentile(x, q=percentile_value), samples_1-samples_2))
+        boot_mean_data_1 = list(map(lambda x: np.percentile(x, q=percentile_value), samples_1))
+        boot_mean_data_2 = list(map(lambda x: np.percentile(x, q=percentile_value), samples_2))
+        bootstrapped_value_1 = np.mean(boot_mean_data_1, q = percentile_value)  
+        bootstrapped_value_2 = np.mean(boot_mean_data_2, q = percentile_value)  
     pd_boot_data = pd.DataFrame(boot_data)   
     left_quant = (1 - bootstrap_conf_level)/2
     right_quant = 1 - (1 - bootstrap_conf_level) / 2
@@ -85,25 +125,35 @@ def bootstrap_test(
         
     # Визуализация
     if chart == True:
-        _, _, bars = plt.hist(pd_boot_data[0], bins = 50)
-        for bar in bars:
-            if abs(bar.get_x()) <= quants.iloc[0][0] or abs(bar.get_x()) >= quants.iloc[1][0]:
-                bar.set_facecolor('red')
-            else: 
-                bar.set_facecolor('grey')
-                bar.set_edgecolor('black')
-
-        plt.style.use('ggplot')
-        plt.vlines(quants,ymin=0,ymax=50,linestyle='--')
-        plt.xlabel('boot_data')
-        plt.ylabel('frequency')
-        plt.title("Histogram of boot_data")
-        plt.show()
+        if chart_type == 'all' or chart_type == 'compare':
+            sns.distplot(boot_mean_data_1, label = 'distribution_1')
+            sns.distplot(boot_mean_data_2, label = 'distribution_2')
+            plt.title("Histograms of ")
+            plt.xlabel('statistic')
+            plt.ylabel('frequency')
+            plt.legend()
+            plt.show()
+            
+        if chart_type == 'all' or chart_type == 'differents':
+            _, _, bars = plt.hist(pd_boot_data[0], bins = 50)
+            for bar in bars:
+                if abs(bar.get_x()) <= quants.iloc[0][0] or abs(bar.get_x()) >= quants.iloc[1][0]:
+                    bar.set_facecolor('red')
+                else: 
+                    bar.set_facecolor('grey')
+                    bar.set_edgecolor('black')
+            plt.vlines(quants,ymin=0,ymax=50,linestyle='--', color = 'blue')
+            plt.xlabel('boot_data')
+            plt.ylabel('frequency')
+            plt.title("Histogram of boot_data")
+            plt.show()
     
     # printing results
     results = pd.DataFrame()
     alpha = 1 - bootstrap_conf_level 
     results.loc[0, 'alpha'] = alpha
+    results.loc[0, 'bootstrapped_value_1'] = bootstrapped_value_1
+    results.loc[0, 'bootstrapped_value_2'] = bootstrapped_value_2
     results.loc[0, 'pvalue'] = p_value
     if print_results==True:
         print('p-значение: ', p_value)
@@ -275,7 +325,7 @@ def get_sample_size_mean_value(std_dev, min_detectable_effect, conf_level = 0.95
         return min_sample_size
     else:
         print('Please, choose one of 3 values of conf_level: 0.9, 0.95 or 0.99') 
-# расчет размера выборки для вычисления среднего значения (непрервый случай) - версия два 
+# расчет размера выборки для вычисления среднего значения (непрерывный случай) - версия два 
 def get_sample_size_mean_value_2(values, min_detectable_effect_pct = 10, power = 0.8, alpha = 0.05):
     from statsmodels.stats.power import TTestIndPower
     std = np.std(values)
